65ebafd0cb4eeec8bd876d82c6150685676186bb
kbmackenzie <kelly.a.betty@gmail.com>
2024-09-23 20:04:13 -0300

    feat: Add 'count-chicken' procedure; use module syntax

---
+++ src/main.scm
@@ -0,0 +1,20 @@
+(module chicken-in-chicken ()
+  (import scheme (chicken base) srfi-13)
+
+  (define chicken-string  "chicken")
+  (define chicken-length  (string-length chicken-word))
+  (define chicken-letters (string->list "chicken"))
+
+  (define (count-chicken line) 
+    (define (count num pos)
+      (cond ((is-chicken line pos) (count (+ 1 num) (+ pos chicken-length)))
+            ((is-whitespace line pos) (count (+ 1 num) (skip-whitespace line pos)))
+            (else "error" #| todo: make this better |#)))
+    (count 0 0))
+
+  (define (sep-lines str)
+    (#| todo |#))
+
+  (define (str-take-while str predicate)
+    (#| todo |#))
+)
