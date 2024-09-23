69523809ed8ecebe4b1055e4fc3262891489f355
kbmackenzie <kelly.a.betty@gmail.com>
2024-09-23 09:06:55 -0300

    feat: Begin work on parser

---
+++ src/parser.scm
@@ -0,0 +1,12 @@
+(import srfi-13)
+
+(define chicken-word "chicken")
+(define chicken-length (string-length chicken-word))
+
+(define (string-drop-while str predicate)
+  (define len (string-length str))
+  (define (drop-rec i)
+    (if (and (< i len) (predicate (string-ref str i)))
+      (drop-rec (+ i 1))
+      (string-drop str i)))
+  (drop-rec 0))
