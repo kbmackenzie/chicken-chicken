222996bf539a71f1873bb483c2b6ee9d93b5ac4d
kbmackenzie <kelly.a.betty@gmail.com>
2024-09-24 06:07:16 -0300

    feat: Add parser type + logic

--- src/main.scm
+++ src/main.scm
@@ -5,17 +5,58 @@
   (define chicken-length  (string-length chicken-word))
   (define chicken-letters (string->list "chicken"))
 
+  #|
+  ------------------------
+  Parser:
+  ------------------------
+  |#
+
   (define-record-type :parser
     (parser result value)
     parser? 
     (result parser-result)
     (value  parser-value))
 
+  (define (parser-success value)
+    (parser "success" value))
+
+  (define (parser-failure value)
+    (parser "failure" value))
+
+  (define (parser-success? p)
+    (string=? (parser-result p) "success"))
+
+  (define (parser-failure? p)
+    (string=? (parser-result p) "failure"))
+
+  ;; Monad 'pure': 
+  (define parser-pure parser-success)
+
+  ;; Monad 'bind': (>>=)
+  (define (parser-bind m f)
+    (if (parser-success? m) (f (parser-value m)) m))
+
+  ;; Monad 'then': (>>)
+  (define (parser-then ma mb)
+    (parser-bind ma (lambda (_) mb)))
+
+
+  #| We can finally begin working on parsing instructions now! |#
   (define (count-chicken line) 
+    (define len (string-length line))
     (define (count num pos)
-      (cond ((is-chicken line pos) (count (+ 1 num) (+ pos chicken-length)))
-            ((is-whitespace line pos) (count (+ 1 num) (skip-whitespace line pos)))
-            (else "error" #| todo: make this better |#)))
+      (cond
+        ((is-chicken line pos)
+          (count (+ 1 num) (+ pos chicken-length)))
+        ((is-whitespace line pos)
+          (count (+ 1 num) (skip-whitespace line pos)))
+        ((>= pos len)
+          (parser-success num))
+        (else
+          (begin
+            (define unrec (string-ref line pos))
+            (define message (sprintf "unrecognized character at ~S: ~S" pos unrec))
+            (parser-failure message)))))
     (count 0 0))
 
   (define (sep-lines str)
