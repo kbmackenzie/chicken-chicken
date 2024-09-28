62a5f55f707341185f206346b8689168d16f0e69
kbmackenzie <kelly.a.betty@gmail.com>
2024-09-28 13:58:04 -0300

    feat(compiler): Generate clearer error message

--- src/compiler.scm
+++ src/compiler.scm
@@ -120,7 +120,7 @@
       (compare-letters chicken-letters start-position)))
 
   (define (generate-error line position)
-    (sprintf "~A: unrecognized character at ~A: ~S"
+    (sprintf "line ~A: unrecognized character at ~A: ~S"
       (+ (car line) 1)
       position
       (string (string-ref (cadr line) position))))
