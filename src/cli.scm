30977f20d38e98d486cf5ae0763742c6811806c7
kbmackenzie <kelly.a.betty@gmail.com>
2024-09-28 12:36:19 -0300

    feat(cli): Enumerate lines when reading

--- src/cli.scm
+++ src/cli.scm
@@ -1,14 +1,18 @@
 (import chicken-in-chicken)
 (import scheme (chicken base) (chicken format) (chicken io) (chicken process-context))
 
+(define (read-lines-enumerated path)
+  (let* ((port     (open-input-file path))
+         (contents (read-lines port)))
+    (close-input-port port)
+    (zip
+      (iota (length contents))
+      contents)))
+
 (for-each
   (lambda (path)
-    (define contents 
-      (let* ((port     (open-input-file path))
-             (contents (read-lines port)))
-        (close-input-port port)
-        contents))
-    (define instructions (parse-instructions contents))
+    (define lines (read-lines-enumerated path))
+    (define instructions (parse-instructions lines))
     (printf "parser ~A: ~S\n"
       (parser-result instructions)
       (if (parser-success? instructions)
