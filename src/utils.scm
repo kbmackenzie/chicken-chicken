041f221f2244afe9dfef128d1e2714f95035716b
kbmackenzie <kelly.a.betty@gmail.com>
2024-11-29 13:55:18 -0300

    feat: Add utils module

---
+++ src/utils.scm
@@ -0,0 +1,12 @@
+(module (chicken-chicken utils) (is-left is-right with-either)
+  (import scheme monad)
+
+  (define (is-left e)
+    (eqv? 'Left (car e)))
+
+  (define (is-right e)
+    (eqv? 'Right (car e)))
+
+  (define (with-either on-left on-right e)
+    (if (is-left e) (on-left (cdr e)) (on-right (cdr e))))
+)
