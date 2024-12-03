8681cfa3a2fe470264fd926be92adf171f13f9ad
kbmackenzie <kelly.a.betty@gmail.com>
2024-12-03 07:40:10 -0300

    feat(compiler): Add better implementation for 'count-chickens', add better error messages

--- src/compiler.scm
+++ src/compiler.scm
@@ -1,10 +1,6 @@
 (module (chicken-chicken compiler) (compile inspect)
-  (import scheme (chicken base) (chicken string) (chicken format) srfi-13 monad)
+  (import scheme (chicken base) (chicken string) (chicken format) srfi-1 srfi-13 monad)
   (import (chicken-chicken vm) (chicken-chicken utils))
-
-  (define chicken-string  "chicken")
-  (define chicken-length  (string-length chicken-string))
-  (define chicken-letters (string->list "chicken"))
 
   ; ------------------------
   ; Instructions:
@@ -60,52 +56,31 @@
   ; Parsing Chicken:
   ; ------------------------
 
-  ; Is the character at the specified position a space?
-  ; returns: boolean
-  (define (is-space str position)
-    (and
-      (< position (string-length str))
-      (eqv? (string-ref str position) #\ )))
-
-  ; Get index of the start of the next token after position.
-  ; returns: number
-  (define (next-token str position)
-    (if (is-space str position) (next-token str (+ 1 position)) position))
-
-  ; Does the input string have the word 'chicken' at the specified position?
-  ; returns: boolean
-  (define (contains-chicken str position)
-    (string=
-      str
-      chicken-string
-      position
-      (min (+ position chicken-length) (string-length str))))
+  ; For this parser, a line is a tuple containing:
+  ;   1st value - line number
+  ;   2nd value - line content
+  ; The line number is important for helpful error messages!
 
   ; Utility for generating syntax error messages.
   ; returns: string
-  (define (syntax-error line position)
-    (sprintf "line ~A: unrecognized character at ~A: ~A"
-      (+ (car line) 1)
-      position
-      (string-ref (cadr line) position)))
+  (define (syntax-error line token)
+    (sprintf "unrecognized token in line ~A: ~A" (+ (car line) 1) token))
 
   ; Count the amount of 'chicken's in a given line.
-  ; returns: number
-  (define (count-chickens line) 
-    (letrec
-      ((line-content (cadr line))
-       (line-length  (string-length (cadr line)))
-       (count
-         (lambda (chickens position)
-           (cond
-             ((contains-chicken line-content position) (count (+ 1 chickens) (+ position chicken-length))  )
-             ((is-space line-content position)         (count chickens (next-token line-content position)) )
-             ((>= position line-length)                (<either>-unit chickens)                            )
-             (else                                     (<either>-fail (syntax-error line position))))))    )
-      (count 0 0)))
+  ; returns: either string number
+  (define (count-chickens line)
+    (fold
+      (lambda (token acc)
+        (do-using <either>
+          (count <- acc)
+          (if (string=? token "chicken")
+            (return (+ 1 count))
+            (fail (syntax-error line token)))))
+      (<either>-unit 0)
+      (string-tokenize (cadr line))))
 
   ; Parse a single instruction.
-  ; returns: instruction
+  ; returns: either string instruction
   (define (parse-instruction lines)
     (do-using <either>
       (opcode  <- (count-chickens (car lines)))
@@ -113,7 +88,7 @@
       (return (instruction opcode operand))))
 
   ; Parse an operand for the 'pick' instruction.
-  ; returns: number
+  ; returns: either string number
   (define (parse-operand lines)
     (do-using <either>
       (if (null? lines)
@@ -121,7 +96,7 @@
         (count-chickens (car lines)))))
 
   ; Parse all instructions.
-  ; returns: list of instructions
+  ; returns: either string (list of instructions)
   (define (parse-instructions lines)
     (if (null? lines)
       (<either>-unit '())
@@ -135,7 +110,7 @@
   ; ------------------------
 
   ; Convert a list of instructions into a list of integers.
-  ; returns: list of numbers
+  ; returns: either string (list of numbers)
   (define (instructions->integers instrs)
     (concat-map
       (lambda (instr)
@@ -145,7 +120,7 @@
       instrs))
 
   ; Compile lines into ES2016-compliant JavaScript.
-  ; returns: string
+  ; returns: either string string
   (define (compile lines)
     (do-using <either>
       (instructions <- (parse-instructions (enumerate-lines lines)))
@@ -155,7 +130,7 @@
         (return output))))
 
   ; Parse instructions and return list of pretty-printed instruction names.
-  ; returns: list of strings
+  ; returns: either string (list of strings)
   (define (inspect lines)
     (do-using <either>
       (instructions <- (parse-instructions (enumerate-lines lines)))
