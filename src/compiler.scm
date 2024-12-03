46bfcce63334bbcc5c91d7927239cf1cd790f3b8
kbmackenzie <kelly.a.betty@gmail.com>
2024-12-03 05:54:58 -0300

    feat(compiler): Simplify 'contains-chicken' function

--- src/compiler.scm
+++ src/compiler.scm
@@ -64,17 +64,12 @@
   (define (skip-spaces str position)
     (if (is-space str position) (skip-spaces str (+ 1 position)) position))
 
-  (define (contains-chicken str at-position)
-    (letrec
-      ((compare-letters
-         (lambda (letters position)
-           (if (null? letters)
-             #t
-             (and
-               (< position (string-length str))
-               (eqv? (string-ref str position) (car letters))
-               (compare-letters (cdr letters) (+ 1 position)))))))
-      (compare-letters chicken-letters at-position)))
+  (define (contains-chicken str position)
+    (string=
+      str
+      chicken-string
+      position
+      (min (+ position chicken-length) (string-length str))))
 
   (define (generate-error line position)
     (sprintf "line ~A: unrecognized character at ~A: ~A"
