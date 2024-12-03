372808be12dc5268538f21ce6591b2425588a1a0
kbmackenzie <kelly.a.betty@gmail.com>
2024-12-03 06:25:31 -0300

    refactor(compiler): Do lots of clean-up, add comments, rename functions

--- src/compiler.scm
+++ src/compiler.scm
@@ -29,16 +29,19 @@
   ; fox:      subtract two top stack values
   ; rooster:  multiply two top stack values
   ; compare:  test equality of two top stack values, push result
-  ; pick:     double-long, load value from stack or user input by index. too complex to sum up
+  ; pick:     double-wide, load value from stack or user input by index. too complex to sum up
   ; peck:     store value from stack in another address. too complex to sum up.
   ; fr:       jump instruction; self-explanatory. moves the instruction pointer.
   ; BBQ:      pop x from stack, push &#x (tl;dr: html-escape char)
   ; push:     pushes number to stack; already explained above
 
-  ; A little util for later.
+  ; Asks if an instruction has an operand.
+  ; returns: boolean
   (define (has-operand? instr)
     (not (eqv? (instruction-operand instr) #f)))
 
+  ; Convert an instruction to a string.
+  ; returns: string
   (define (instruction->string instr)
     (case (instruction-opcode instr)
       ((0) "axe"     )
@@ -56,14 +59,21 @@
   ; ------------------------
   ; Parsing Chicken:
   ; ------------------------
+
+  ; Is the character at the specified position a space?
+  ; returns: boolean
   (define (is-space str position)
     (and
       (< position (string-length str))
       (eqv? (string-ref str position) #\ )))
 
-  (define (skip-spaces str position)
-    (if (is-space str position) (skip-spaces str (+ 1 position)) position))
+  ; Get index of the start of the next token after position.
+  ; returns: number
+  (define (next-token str position)
+    (if (is-space str position) (next-token str (+ 1 position)) position))
 
+  ; Does the input string have the word 'chicken' at the specified position?
+  ; returns: boolean
   (define (contains-chicken str position)
     (string=
       str
@@ -71,13 +81,17 @@
       position
       (min (+ position chicken-length) (string-length str))))
 
-  (define (generate-error line position)
+  ; Utility for generating syntax error messages.
+  ; returns: string
+  (define (syntax-error line position)
     (sprintf "line ~A: unrecognized character at ~A: ~A"
       (+ (car line) 1)
       position
       (string-ref (cadr line) position)))
 
-  (define (count-chicken line) 
+  ; Count the amount of 'chicken's in a given line.
+  ; returns: number
+  (define (count-chickens line) 
     (letrec
       ((line-content (cadr line))
        (line-length  (string-length (cadr line)))
@@ -85,23 +99,29 @@
          (lambda (chickens position)
            (cond
              ((contains-chicken line-content position) (count (+ 1 chickens) (+ position chicken-length))  )
-             ((is-space line-content position)         (count chickens (skip-spaces line-content position)))
+             ((is-space line-content position)         (count chickens (next-token line-content position)) )
              ((>= position line-length)                (<either>-unit chickens)                            )
-             (else                                     (<either>-fail (generate-error line position))))))  )
+             (else                                     (<either>-fail (syntax-error line position))))))    )
       (count 0 0)))
 
+  ; Parse a single instruction.
+  ; returns: instruction
   (define (parse-instruction lines)
     (do-using <either>
-      (opcode  <- (count-chicken (car lines)))
+      (opcode  <- (count-chickens (car lines)))
       (operand <- (if (= opcode 6) (parse-operand (cdr lines)) (return #f)))
       (return (instruction opcode operand))))
 
+  ; Parse an operand for the 'pick' instruction.
+  ; returns: number
   (define (parse-operand lines)
     (do-using <either>
       (if (null? lines)
         (fail "expected operand; got end of input")
-        (count-chicken (car lines)))))
+        (count-chickens (car lines)))))
 
+  ; Parse all instructions.
+  ; returns: list of instructions
   (define (parse-instructions lines)
     (if (null? lines)
       (<either>-unit '())
@@ -113,6 +133,9 @@
   ; ------------------------
   ; Compiling Chicken:
   ; ------------------------
+
+  ; Convert a list of instructions into a list of integers.
+  ; returns: list of numbers
   (define (instructions->integers instrs)
     (concat-map
       (lambda (instr)
@@ -121,6 +144,8 @@
           (list (instruction-opcode instr))))
       instrs))
 
+  ; Compile lines into ES2016-compliant JavaScript.
+  ; returns: string
   (define (compile lines)
     (do-using <either>
       (instructions <- (parse-instructions (enumerate-lines lines)))
@@ -129,6 +154,8 @@
              (output   (sprintf "~A;const instructions=[~A];" vm js-array)))
         (return output))))
 
+  ; Parse instructions and return list of pretty-printed instruction names.
+  ; returns: list of strings
   (define (inspect lines)
     (do-using <either>
       (instructions <- (parse-instructions (enumerate-lines lines)))
